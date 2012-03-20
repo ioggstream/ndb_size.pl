6f1cd07bfdd9334d4d8af0260577e449424358ff
Roberto Polli <robipolli@gmail.com>
2012-03-20 17:06:33 +0100

    fix 62635

--- ndb_size.pl
+++ ndb_size.pl
@@ -462,7 +462,11 @@ my %queries; # used for loadqueries/savequeries
 
 if(!$loadqueries)
 {
-    my $dsn = "DBI:mysql:host=$hostname";
+    my ($host,$port) = split(/:/, $hostname);
+    my $dsn = "DBI:mysql:host=$host";
+    if (defined($port)) {
+       $dsn .=";port=$port";
+    }
     $dsn.= ";mysql_socket=$socket" if ($socket);
     $dbh= DBI->connect($dsn, $user, $password) or exit(1);
     $report->dsn($dsn);
